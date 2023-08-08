d8278c2881a9968f91ff89962d3c2f05eeb62529
Abhishek Rajput <abhishekrajput.lamprostech@gmail.com>
2023-08-08 10:32:53 +0530

    Completed Verify User Page

--- src/components/issuingAuthorityDashboard/IAUserRequest.js
+++ src/components/issuingAuthorityDashboard/IAUserRequest.js
@@ -1,8 +1,59 @@
 import React from 'react'
+import '../../styles/issuingAuthorityDashboard/IAUserRequest.css'
+import userImage from '../../assets/dummy-user.png'
 
 function IAUserRequest() {
+  const userRequests = [
+    {
+      id: 'request001',
+      certificateTitle: 'SSC Certificate',
+      userName: 'John Doe',
+      userId: 'john001',
+      requestDate: '2023-08-20',
+      requestType: 'Issue',
+    },
+    {
+      id: 'request002',
+      certificateTitle: 'HSC Certificate',
+      userName: 'Jane Smith',
+      userId: 'jane002',
+      requestDate: '2023-08-19',
+      requestType: 'Verify',
+    },
+  ];
   return (
-    <div>IAUserRequest</div>
+    <div className="ia-user-requests-container">
+      <div className="ia-user-requests">
+      <div className="ia-user-request ia-user-request-heading">
+          <div className="ia-request-details">
+            <div className="ia-field ia-serial-number">Serial No.</div>
+            <div className="ia-field ia-user-profile-photo">User Photo</div>
+            <div className="ia-field ia-certificate-title">Certificate Title</div>
+            <div className="ia-field ia-user-info-name">User Name</div>
+            <div className="ia-field ia-user-info-id">User ID</div>
+            <div className="ia-field ia-request-date">Request Date</div>
+            <div className="ia-field ia-request-type">Request Type</div>
+          </div>
+          <div >Actions</div>
+        </div>
+        {userRequests.map((request, index) => (
+           <div key={request.id} className="ia-user-request">
+           <div className="ia-request-details">
+             <div className="ia-field ia-serial-number">{index + 1}</div>
+             <div className="ia-field ia-user-profile-photo">
+               <img src={userImage} alt="User Profile" />
+             </div>
+             <div className="ia-field ia-certificate-title">{request.certificateTitle}</div>
+             <div className="ia-field ia-user-info-name">{request.userName}</div>
+             <div className="ia-field ia-user-info-id">{request.userId}</div>
+             <div className="ia-field ia-request-date">{request.requestDate}</div>
+             <div className="ia-field ia-request-type">{request.requestType}</div>
+           </div>
+           <div className="ia-view-button">View</div>
+         </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/components/issuingAuthorityDashboard/TrackAlumni.js
+++ src/components/issuingAuthorityDashboard/TrackAlumni.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../../styles/issuingAuthorityDashboard/TrackAlumni.css'
 import logo from '../../assets/dummy-user.png'
 
@@ -11,34 +11,61 @@ function TrackAlumni() {
       profilePhoto: logo,
     },
     {
-      id: 'alumni002',
+      id: 'alumni001',
       name: 'Jane Smith',
       email: 'jane.smith@example.com',
       profilePhoto: logo,
     },
     {
-      id: 'alumni001',
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-      profilePhoto: logo,
-    },
-    {
-      id: 'alumni001',
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-      profilePhoto: logo,
-    },
-    {
-      id: 'alumni001',
+      id: 'alumni003',
       name: 'John Doe',
       email: 'john.doe@example.com',
       profilePhoto: logo,
     },
   ];
+
+  const [searchId, setSearchId] = useState('');
+  const [searchName, setSearchName] = useState('');
+  const [searchEmail, setSearchEmail] = useState('');
+
+  const filteredAlumniData = initialAlumniData.filter((alumni) => {
+    return (
+      alumni.id.toLowerCase().includes(searchId.toLowerCase()) &&
+      alumni.name.toLowerCase().includes(searchName.toLowerCase()) &&
+      alumni.email.toLowerCase().includes(searchEmail.toLowerCase())
+    );
+  });
+
   return (
     <div>
+      <div className="search-filters">
+        <div className="search-filter">
+          <input
+            type="text"
+            placeholder="Search by ID"
+            value={searchId}
+            onChange={(e) => setSearchId(e.target.value)}
+          />
+        </div>
+        <div className="search-filter">
+          <input
+            type="text"
+            placeholder="Search by Name"
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
+          />
+        </div>
+        <div className="search-filter">
+          <input
+            type="text"
+            placeholder="Search by Email"
+            value={searchEmail}
+            onChange={(e) => setSearchEmail(e.target.value)}
+          />
+        </div>
+      </div>
     <div className="alumni-list">
-      {initialAlumniData.map((alumni) => (
+      {filteredAlumniData.map((alumni) => (
         <div key={alumni.id} className="alumni-card">
           <div className="alumni-profile-photo">
             <img src={alumni.profilePhoto} alt="User Profile" />
