04b47d33091ad1cd9fbfb370300c5c76161a4be4
saurabh142001 <saurabhjha1402@gmail.com>
2023-08-09 11:16:22 +0530

    modified code

--- src/components/LandingPage/GetStarted.jsx
+++ src/components/LandingPage/GetStarted.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import getstarted from "/Users/saurabhjha1402/Desktop/TraceCheck/src/assets/getstarted.png";
+import image from "/Users/saurabhjha1402/Desktop/TraceCheck/src/assets/getstarted.svg";
 
 const GetStarted = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light navbar">
+      <nav className="navbar navbar-expand-lg navbar-light ">
         <div className="container">
           <a className="navbar-brand" href="#">
             <img
-              src=" image"
+              src="logo" // Corrected the image source
               alt="Logo"
               width="50"
               height="50"
@@ -30,36 +30,38 @@ const GetStarted = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <button className="btn btn-outline-primary me-2">Login</button>
+                <button className="btn btn-outline-dark me-2">Login</button>
               </li>
               <li className="nav-item">
-                <button className="btn btn-primary">Sign Up</button>
+                <button className="btn btn-dark btng ">Sign Up</button>
               </li>
             </ul>
           </div>
         </div>
       </nav>
       <div className="container mt-5 vh-100">
-        <div className="row h-200">
+        <div className="row h-100">
+          {" "}
+          {/* Changed h-200 to h-100 */}
           <div className="col-md-6">
             <div className="mx-auto" style={{ maxWidth: "800px" }}>
               <img
-                src="/Users/saurabhjha1402/Desktop/TraceCheck/src/assets/getstarted.png"
+                src={image} // Corrected the image source
                 alt="Image"
-                className=" img-fluid w-100 "
+                className="img-fluid w-100"
               />
             </div>
           </div>
           <div className="col-md-6 d-flex flex-column justify-content-center">
-            <h1 className="fs-3">Name of the issuing authority </h1>
+            <h1 className="fs-3">Name of the issuing authority</h1>
             <p className="fs-5">Issuing authority details</p>
-            <button className="btn btn-primary" style={{ maxWidth: "200px" }}>
+            <button className="btn btn-dark btng" style={{ maxWidth: "200px" }}>
               Get Started
             </button>
           </div>
         </div>
       </div>
-      <footer className="bg-secondary text-center text-white py-3">
+      <footer className="bg-secondary text-center text-white py-8">
         <div className="container">
           <p className="mb-0">
             &copy; {new Date().getFullYear()} Your Company. All rights reserved.
--- src/components/RegistrationPage/RegistrationUser.jsx
+++ src/components/RegistrationPage/RegistrationUser.jsx
@@ -16,61 +16,70 @@ function RegistrationUser() {
   };
 
   return (
-    <section>
-      <div className={`page ${isLoginActive ? "active" : ""}`}>
-        <div className={`welcome ${isLoginActive ? "active" : ""}`}>
-          <h2>Welcome Back!</h2>
-          <p>Hello</p>
-          {isLoginActive ? null : (
-            <button className="sign_in" onClick={handleSignInClick}>
-              Sign In
+    <div classname="bgmain">
+      <section>
+        <div className={`page ${isLoginActive ? "active" : ""}`}>
+          <div className={`welcome ${isLoginActive ? "active" : ""}`}>
+            <h2>Welcome Back!</h2>
+            <p>Hello</p>
+            {isLoginActive ? null : (
+              <button className="sign_in" onClick={handleSignInClick}>
+                Sign In
+              </button>
+            )}
+            <button
+              className={`btn ${isSignUpActive ? "active" : ""}`}
+              onClick={handleSignUpClick}
+            >
+              Sign Up
             </button>
-          )}
-          <button
-            className={`btn ${isSignUpActive ? "active" : ""}`}
-            onClick={handleSignUpClick}
-          >
-            Sign Up
-          </button>
+          </div>
+          <div className={`sign_up ${isSignUpActive ? "active" : ""}`}>
+            <form method="POST" action="signup_user.php">
+              <h2>Sign Up your Account</h2>
+              <input type="text" name="name" placeholder="Full Name" required />
+              <br />
+              <input type="email" name="email" placeholder="Email" required />
+              <br />
+              <input
+                type="file" /* Add the input for profile photo */
+                name="profilePhoto"
+                accept="image/*"
+                placeholder="Profile Photo"
+                required
+              />
+              <br />
+              <input
+                type="submit"
+                name="sign_up"
+                value="Sign Up"
+                className="up"
+              />
+            </form>
+          </div>
+          <div className={`login ${isLoginActive ? "active" : ""}`}>
+            <form method="POST" action="login_user.php">
+              <h2>Login your Account</h2>
+              <input type="email" name="email" placeholder="Email" required />
+              <br />
+              <input
+                type="submit"
+                name="sign_in"
+                value="Sign In"
+                className="in"
+              />
+            </form>
+          </div>
         </div>
-        <div className={`sign_up ${isSignUpActive ? "active" : ""}`}>
-          <form method="POST" action="signup_user.php">
-            <h2>Sign Up your Account</h2>
-            <input type="text" name="name" placeholder="Full Name" required />
-            <br />
-            <input type="email" name="email" placeholder="Email" required />
-            <br />
-            <input
-              type="file" /* Add the input for profile photo */
-              name="profilePhoto"
-              accept="image/*"
-              placeholder="Profile Photo"
-              required
-            />
-            <br />
-            <input
-              type="submit"
-              name="sign_up"
-              value="Sign Up"
-              className="up"
-            />
-          </form>
+      </section>
+      <footer className="bg-secondary text-center text-white py-8">
+        <div className="container">
+          <p className="mb-0">
+            &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+          </p>
         </div>
-        <div className={`login ${isLoginActive ? "active" : ""}`}>
-          <form method="POST" action="login_user.php">
-            <h2>Login your Account</h2>
-            <input type="email" name="email" placeholder="Email" required />
-            <br />
-            <input
-              type="submit"
-              name="sign_in"
-              value="Sign In"
-              className="in"
-            />
-          </form>
-        </div>
-      </div>
-    </section>
+      </footer>
+    </div>
   );
 }
 
