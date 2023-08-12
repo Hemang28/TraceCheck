f119ed7464a4800f0647ee0dd808c71ccfc07e41
Saurabh <saurabhjha1402@gmail.com>
2023-08-12 11:57:21 +0530

    IA certificate and User pages design changes

--- src/components/issuingAuthorityDashboard/IACertificateModal.js
+++ src/components/issuingAuthorityDashboard/IACertificateModal.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import '../../styles/issuingAuthorityDashboard/IACertificateModal.css'
+import { EAS } from "@ethereum-attestation-service/eas-sdk";
+import { EAS, Offchain, SchemaEncoder, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
+
 
 function IACertificateModal({ certificate, onClose }) {
+   const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+
+// Initialize the sdk with the address of the EAS Schema contract address
+const eas = new EAS(EASContractAddress);
+
+// Gets a default provider (in production use something else like infura/alchemy)
+const provider = ethers.providers.getDefaultProvider(
+  "sepolia"
+);
+
+// Connects an ethers style provider/signingProvider to perform read/write functions.
+// MUST be a signer to do write operations!
+eas.connect(provider);
+
+
+
+
   return (
     <div className="modal-backdrop">
     <div className="modal-overlay">
