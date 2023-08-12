5790b9efe935fa0e44687c7a7edfa13ba1c45af1
Abhishek Rajput <abhishekrajput.lamprostech@gmail.com>
2023-08-12 18:12:13 +0530

    Modified Landing To page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -15,6 +15,22 @@ const App = () => {
     navigate("/getstarted");
   };
 
+  // Sponsors data
+  const sponsors = [
+    {
+      title: "Base",
+      description: "Base is a secure, low-cost, builder-friendly Ethereum L2 built to bring the next billion users onchain.Base is incubated within Coinbase and plans to progressively decentralize in the years ahead. We believe that decentralization is critical to creating an open, global cryptoeconomy that is accessible to everyone.",
+    },
+    {
+      title: "Safe",
+      description: "Safe brings digital ownership of accounts to everyone by building universal and open contract standards for the custody of digital assets, data, and identity.Safe is at the forefront of modular Smart Account infrastructure, paving the way for developers to create a diverse range of applications and wallets.",
+    },
+    {
+      title: "EAS",
+      description: "EAS is a public good, available for anyone to use and build upon. It aims to create a common layer for attestations that can be used and integrated by a variety of different applications and protocols. This will enable the creation of a universal layer for attestations used to verify the authenticity and integrity.",
+    },
+  ];
+
   return (
     <div>
       <div
@@ -33,20 +49,36 @@ const App = () => {
                 <img src={imageL} alt="Image" className="img-fluid" />
               </div>
             </div>
-            <div className="col-md-6 d-flex flex-column justify-content-center align-items-start ">
-              <div className={` h2  ${showContent ? "fade-in" : ""}`}>
+            <div className="col-md-6 d-flex flex-column justify-content-center align-items-start">
+              <div className={`h2 ${showContent ? "fade-in" : ""}`}>
                 <br />
                 <h1>TraceCheck</h1>
-                <h5 style={{textAlign:"justify"}}>
-                Elevating Trust, One Verified Credential at a Time
-                Blockchain Security for Transparent, Reliable Background Checks
-                Empower Your Verification Process with TRACECHECK's Decentralized Solutions
+                <h5 style={{ textAlign: "justify" }}>
+                  Elevating Trust, One Verified Credential at a Time
+                  Blockchain Security for Transparent, Reliable Background Checks
+                  Empower Your Verification Process with TRACECHECK's Decentralized Solutions
                 </h5>
-                <br></br>
-                
+                <br />
                 <button className="btn btn-dark btng" onClick={handleGetStarted}>Get Started</button>
               </div>
             </div>
+          </div>
+        </div>
+        {/* Title for sponsors */}
+        <div className="container mt-5 text-center">
+          <h2 className="sponsor-title" style={{ color: "#ff592c" }}>Sponsors Used</h2>
+        </div>
+        {/* Sponsors */}
+        <div className="container mt-3">
+          <div className="row">
+            {sponsors.map((sponsor, index) => (
+              <div key={index} className="col-md-4 mb-4">
+                <div className="sponsor" style={{ backgroundColor: "#f5f5f5" }}>
+                  <h3 className="sponsor-title" style={{ color: "#ff592c" }}>{sponsor.title}</h3>
+                  <p className="sponsor-description" style={{ color: "#666" }}>{sponsor.description}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
@@ -55,3 +87,5 @@ const App = () => {
 };
 
 export default App;
+
+
