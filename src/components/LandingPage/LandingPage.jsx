809f6215f3c9528809947c73c369cf489cae7d49
Abhishek Rajput <abhishekrajput.lamprostech@gmail.com>
2023-08-16 09:02:40 +0530

    Updated Landing Page Code

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -15,22 +15,6 @@ const App = () => {
     navigate("/getstarted");
   };
 
-  // Sponsors data
-  const sponsors = [
-    {
-      title: "Base",
-      description: "Base is a secure, low-cost, builder-friendly Ethereum L2 built to bring the next billion users onchain.Base is incubated within Coinbase and plans to progressively decentralize in the years ahead. We believe that decentralization is critical to creating an open, global cryptoeconomy that is accessible to everyone.",
-    },
-    {
-      title: "Safe",
-      description: "Safe brings digital ownership of accounts to everyone by building universal and open contract standards for the custody of digital assets, data, and identity.Safe is at the forefront of modular Smart Account infrastructure, paving the way for developers to create a diverse range of applications and wallets.",
-    },
-    {
-      title: "EAS",
-      description: "EAS is a public good, available for anyone to use and build upon. It aims to create a common layer for attestations that can be used and integrated by a variety of different applications and protocols. This will enable the creation of a universal layer for attestations used to verify the authenticity and integrity.",
-    },
-  ];
-
   return (
     <div>
       <div
@@ -62,23 +46,6 @@ const App = () => {
                 <button className="btn btn-dark btng" onClick={handleGetStarted}>Get Started</button>
               </div>
             </div>
-          </div>
-        </div>
-        {/* Title for sponsors */}
-        <div className="container mt-5 text-center">
-          <h2 className="sponsor-title" style={{ color: "#ff592c" }}>Sponsors Used</h2>
-        </div>
-        {/* Sponsors */}
-        <div className="container mt-3">
-          <div className="row">
-            {sponsors.map((sponsor, index) => (
-              <div key={index} className="col-md-4 mb-4">
-                <div className="sponsor" style={{ backgroundColor: "#f5f5f5" }}>
-                  <h3 className="sponsor-title" style={{ color: "#ff592c" }}>{sponsor.title}</h3>
-                  <p className="sponsor-description" style={{ color: "#666" }}>{sponsor.description}</p>
-                </div>
-              </div>
-            ))}
           </div>
         </div>
       </div>
